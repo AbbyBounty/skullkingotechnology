8b732234fb792b27999c38cc88d4c88a6ebea1e3
abhilash <abhilash.kamble376@gmail.com>
2021-05-26 21:37:46 +0530

    updated header

---
+++ script/script.js
@@ -0,0 +1,22 @@
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
+
+
+/* Demo purposes only */
+$(".hover").mouseleave(
+    function () {
+        $(this).removeClass("hover");
+    }
+);
