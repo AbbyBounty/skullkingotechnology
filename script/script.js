93f3718fa9e7239a8e90f15c24f3a81cc1aba2f0
abhilash <abhilash.kamble376@gmail.com>
2021-06-01 20:50:34 +0530

    onScroll count change

--- script/script.js
+++ script/script.js
@@ -1,17 +1,3 @@
-/* Demo purposes only */
-$(".hover").mouseleave(
-    function () {
-        $(this).removeClass("hover");
-    }
-);
-
-
-/* Demo purposes only */
-$(".hover").mouseleave(
-    function () {
-        $(this).removeClass("hover");
-    }
-);
 
 
 /* Demo purposes only */
@@ -32,3 +18,42 @@ window.addEventListener('scroll', () => {
     scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
 });
 
+
+var a = 0
+$(window).scroll(function () {
+    var oTop = $("#projectFacts").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $.fn.jQuerySimpleCounter = function (options) {
+            var settings = $.extend({
+                start: 0,
+                end: 100,
+                easing: 'swing',
+                duration: 4000,
+                complete: ''
+            }, options);
+
+            var thisElement = $(this);
+
+            $({ count: settings.start }).animate({ count: settings.end }, {
+                duration: settings.duration,
+                easing: settings.easing,
+                step: function () {
+                    var mathCount = Math.ceil(this.count);
+                    thisElement.text(mathCount);
+                },
+                complete: settings.complete
+            });
+        };
+
+
+        $('#number1').jQuerySimpleCounter({ end: 12, duration: 3000 });
+        $('#number2').jQuerySimpleCounter({ end: 55, duration: 3000 });
+        $('#number3').jQuerySimpleCounter({ end: 359, duration: 2000 });
+        $('#number4').jQuerySimpleCounter({ end: 246, duration: 2500 });
+
+
+
+
+        a = 1;
+    }
+})
