505fadc9c65a65ffde71847114ac1905885276e1
abhilash <abhilash.kamble376@gmail.com>
2021-08-23 22:22:13 +0530

    modified navbar ,banner features

--- src/carousel.js
+++ src/carousel.js
@@ -3,6 +3,8 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 // import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import wifi from './assets/wifi.png'
+import tab from './assets/tab.png'
+import cyborg88 from './assets/cyborg-88.png'
 
 const navigation = [
     { name: 'Product', href: '#' },
@@ -13,7 +15,7 @@ const navigation = [
 
 export default function carousal() {
     return (
-        <div className="relative bg-white overflow-hidden">
+        <div className="relative  " >
             <div className="max-w-7xl mx-auto">
                 <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
                     <svg
@@ -26,29 +28,14 @@ export default function carousal() {
                         <polygon points="50,0 100,0 50,100 0,100" />
                     </svg>
 
-                    <Popover>
-                        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
 
-                        </div>
 
-                        <Transition
-                            as={Fragment}
-                            enter="duration-150 ease-out"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
-                            leave="duration-100 ease-in"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        >
-
-                        </Transition>
-                    </Popover>
 
                     <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div className="sm:text-center lg:text-left">
                             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                                 <span className="block xl:inline">Data to enrich your</span>{' '}
-                                <span className="block text-indigo-600 xl:inline">online business</span>
+                                <span className="block text-indigo-600 xl:inline">online connectivity</span>
                             </h1>
                             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -87,6 +74,7 @@ export default function carousal() {
                     alt=""
                 />
             </div>
+
         </div>
     )
 }
